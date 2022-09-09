a9c8e8030898b8d7524c174c86bd33382a195fa2
Rumeet Goradia <rumeet.goradia@gmail.com>
2022-09-09 11:22:07 -0400

    Fix env stuff

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,25 +1,25 @@
+import { env } from "@/env/server.mjs";
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-import { env } from "../../../env/server.mjs";
+import SpotifyProvider from "next-auth/providers/spotify";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
-  // Configure one or more authentication providers
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
+	// Include user.id on session
+	callbacks: {
+		session({ session, user }) {
+			if (session.user) {
+				session.user.id = user.id;
+			}
+			return session;
+		},
+	},
+	// Configure one or more authentication providers
+	providers: [
+		SpotifyProvider({
+			clientId: env.SPOTIFY_CLIENT_ID,
+			clientSecret: env.SPOTIFY_CLIENT_SECRET,
+		}),
+		// ...add more providers here
+	],
 };
 
 export default NextAuth(authOptions);
