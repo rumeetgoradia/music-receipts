f812b6b4e53e017bce5f1c762743af92c86f5ebc
Rumeet Goradia <rumeet.goradia@gmail.com>
2022-09-15 16:10:01 -0400

    Update popularity calculation

--- src/utils/artist.ts
+++ src/utils/artist.ts
@@ -1,13 +1,19 @@
 import { Artist } from "spotify-web-api-ts/types/types/SpotifyObjects";
 export const getFormattedPopularity = (artist: Artist): string => {
-	return (artist.popularity / 10.0).toFixed(2);
+	return calculatePopularity(artist.popularity, artist.genres.length).toFixed(
+		2
+	);
 };
 
 export const getTotalPopularity = (artists: Artist[]): string => {
 	let popularity = 0;
 	for (const artist of artists) {
-		popularity += artist.popularity;
+		popularity += calculatePopularity(artist.popularity, artist.genres.length);
 	}
 
 	return popularity.toFixed(2);
+};
+
+const calculatePopularity = (popularity: number, genres: number): number => {
+	return Math.max(1, (10 - popularity / 10.0) * 10) + genres / 10.0;
 };
